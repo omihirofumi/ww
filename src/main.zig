08a30382ab53b7203316f62c96f84e63caa3ce78
omihirofumi <99390907+omihirofumi@users.noreply.github.com>
2025-12-30 17:27:52 +0900

    feat: try to get args

--- src/main.zig
+++ src/main.zig
@@ -1,27 +1,24 @@
 const std = @import("std");
 const ww = @import("ww");
 
+var writer_buf: [1024]u8 = undefined;
+var stdout_writer = std.fs.File.stdout().writer(&writer_buf);
+const stdout = &stdout_writer.interface;
+
+var error_buf: [1024]u8 = undefined;
+var stderr_writer = std.fs.File.stderr().writer(&error_buf);
+const stderr = &stderr_writer.interface;
+
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try ww.bufferedPrint();
-}
-
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+    // read args
+    var args_iter = std.process.args();
+    if (args_iter.inner.count != 3) {
+        try stderr.print("usage: ", .{});
+        try stderr.flush();
+        return;
+    }
+    while (args_iter.next()) |val| {
+        try stdout.print("args: {s}", .{val});
+    }
+    try stdout.flush();
 }
