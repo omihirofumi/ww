690a4bccb021d17179bdf5d6a8ac75bf239b2ada
omihirofumi <99390907+omihirofumi@users.noreply.github.com>
2025-12-31 12:16:34 +0900

    refactor: create new module ww.zig and move related functions from main.zig

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
-const ww = @import("ww");
+const ww = @import("ww.zig");
 
 var writer_buf: [1024]u8 = undefined;
 var stdout_writer = std.fs.File.stdout().writer(&writer_buf);
@@ -17,79 +17,17 @@ pub fn main() !void {
     var args = std.process.args();
     _ = args.next();
 
-    const subcommand = args.next() orelse {
+    var app = ww.App{ .allocator = allocator };
+
+    const cmd = app.parse(&args) catch {
         try printUsage();
         return;
     };
 
-    if (std.mem.eql(u8, subcommand, "new")) {
-        const name = args.next() orelse {
-            try printUsage();
-            return;
-        };
-
-        const repo_root = try jjRoot(allocator);
-        defer allocator.free(repo_root);
-
-        const repo_name = std.fs.path.basename(repo_root);
-        const home = std.posix.getenv("HOME") orelse return error.MissingHome;
-
-        const target_path = try std.fs.path.join(allocator, &.{
-            home,
-            ".jj-workspace",
-            repo_name,
-            name,
-        });
-        defer allocator.free(target_path);
-
-        try runJjWorkspaceAdd(allocator, target_path);
-        return;
-    }
-    try printUsage();
+    try app.run(cmd);
 }
 
 fn printUsage() !void {
     try stderr.print("usage: ww new <name>\n", .{});
     try stderr.flush();
-}
-
-fn jjRoot(allocator: std.mem.Allocator) ![]const u8 {
-    var child = std.process.Child.init(&.{ "jj", "root" }, allocator);
-    child.stdout_behavior = .Pipe;
-    child.stderr_behavior = .Inherit;
-
-    try child.spawn();
-
-    const stdout_file = child.stdout orelse return error.NoStdout;
-
-    var io_buf: [1024]u8 = undefined;
-    var file_reader = stdout_file.reader(&io_buf);
-    const r = &file_reader.interface;
-
-    const out = try r.allocRemaining(allocator, .unlimited);
-    defer allocator.free(out);
-
-    const term = try child.wait();
-    switch (term) {
-        .Exited => |code| if (code != 0) return error.JjRootFailed,
-        else => return error.JjRootFailed,
-    }
-
-    const trimmed = std.mem.trimEnd(u8, out, "\r\n");
-
-    return try allocator.dupe(u8, trimmed);
-}
-
-fn runJjWorkspaceAdd(allocator: std.mem.Allocator, path: []const u8) !void {
-    var child = std.process.Child.init(&.{ "jj", "workspace", "add", path }, allocator);
-    child.stdin_behavior = .Inherit;
-    child.stdout_behavior = .Inherit;
-    child.stderr_behavior = .Inherit;
-
-    try child.spawn();
-    const term = try child.wait();
-    switch (term) {
-        .Exited => |code| if (code != 0) return error.runJjWorkspaceAddFailed,
-        else => return error.runJjWorkspaceAddFailed,
-    }
 }
---
+++ src/ww.zig
@@ -0,0 +1,95 @@
+const std = @import("std");
+
+const Command = union(enum) {
+    new: []const u8,
+};
+
+pub const App = struct {
+    allocator: std.mem.Allocator,
+
+    pub fn parse(self: App, args: *std.process.ArgIterator) !Command {
+        _ = self;
+        const subcommand = args.next() orelse return error.InvalidArgs;
+
+        if (std.mem.eql(u8, subcommand, "new")) {
+            const name = args.next() orelse return error.InvalidArgs;
+            return .{ .new = name };
+        }
+
+        // if (std.mem.eql(u8, subcommand, "go")) {
+        //     const name = args.next() orelse return error.InvalidArgs;
+        //     return .{ .go = name };
+        // }
+
+        return error.InvalidArgs;
+    }
+
+    pub fn run(self: App, cmd: Command) !void {
+        switch (cmd) {
+            .new => |name| try self.runNew(name),
+        }
+    }
+
+    fn runNew(self: App, name: []const u8) !void {
+        const repo_root = try jjRoot(self.allocator);
+        defer self.allocator.free(repo_root);
+
+        const workspacePath = try buildWorkspacePath(self.allocator, repo_root, name);
+        defer self.allocator.free(workspacePath);
+
+        try runJjWorkspaceAdd(self.allocator, workspacePath);
+    }
+};
+
+fn buildWorkspacePath(allocator: std.mem.Allocator, repo_root: []const u8, name: []const u8) ![]const u8 {
+    const repo_name = std.fs.path.basename(repo_root);
+    const home = std.posix.getenv("HOME") orelse return error.MissingHome;
+
+    return try std.fs.path.join(allocator, &.{
+        home,
+        ".jj-workspace",
+        repo_name,
+        name,
+    });
+}
+
+fn jjRoot(allocator: std.mem.Allocator) ![]const u8 {
+    var child = std.process.Child.init(&.{ "jj", "root" }, allocator);
+    child.stdout_behavior = .Pipe;
+    child.stderr_behavior = .Inherit;
+
+    try child.spawn();
+
+    const stdout_file = child.stdout orelse return error.NoStdout;
+
+    var io_buf: [1024]u8 = undefined;
+    var file_reader = stdout_file.reader(&io_buf);
+    const r = &file_reader.interface;
+
+    const out = try r.allocRemaining(allocator, .unlimited);
+    defer allocator.free(out);
+
+    const term = try child.wait();
+    switch (term) {
+        .Exited => |code| if (code != 0) return error.JjRootFailed,
+        else => return error.JjRootFailed,
+    }
+
+    const trimmed = std.mem.trimEnd(u8, out, "\r\n");
+
+    return try allocator.dupe(u8, trimmed);
+}
+
+fn runJjWorkspaceAdd(allocator: std.mem.Allocator, path: []const u8) !void {
+    var child = std.process.Child.init(&.{ "jj", "workspace", "add", path }, allocator);
+    child.stdin_behavior = .Inherit;
+    child.stdout_behavior = .Inherit;
+    child.stderr_behavior = .Inherit;
+
+    try child.spawn();
+    const term = try child.wait();
+    switch (term) {
+        .Exited => |code| if (code != 0) return error.runJjWorkspaceAddFailed,
+        else => return error.runJjWorkspaceAddFailed,
+    }
+}
